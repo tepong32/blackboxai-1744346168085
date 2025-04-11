84eaf42fa2efe374c990f25a6a797329cf98afed
tepong32 <tepong32@gmail.com>
2025-04-11 13:09:20 +0800

    Add accounts/urls.py 1744348159454

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('bulk-create/', views.bulk_user_create, name='bulk_user_create'),
+]
